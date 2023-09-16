47b8b9533c4471a240da65f10f8c11387cd791f2
ridham <ridhamthumar24@gmail.com>
2023-09-16 12:42:57 +0530

    Initial commit

---
+++ src/test/java/io/ridham/walletapp/WalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ridham.walletapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
